6e38f7ec2790e7c7570f994c28e78789e835f3d8
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-06-15 13:29:23 -0400

    created a function that takes a license and uses a switch to return that license`s badge

--- Develop/index.js
+++ Develop/index.js
@@ -41,9 +41,16 @@ const questions = [
     }
     ,
     {
-        type: 'input',
-        message: 'Write your License:',
-        name: 'License'
+        type: 'list',
+        message: 'Choose your License:',
+        name: 'License',
+        choices: [  "MIT License",
+        "Apache License 2.0",
+        "BSD 2-Clause License",
+        "BSD 3-Clause License",
+        "Mozilla Public License (MPL)",
+        "zlib License",
+        "Eclipse Public License"]
     },
     {
         type: 'input',
@@ -62,8 +69,6 @@ const questions = [
     }
 ];
 
-let exit = false; //this is a flag to track if the user exits or not.
-
 // TODO: Create a function to initialize app 
 function init() {
     inquirer.prompt(startConfirmation).then((response) => {
@@ -71,16 +76,43 @@ function init() {
         if (response.initialization) {
             fs.writeFileSync("README.md", README(response))     //creates a README file with undefined content. this is just a skeleton of the file contents
             console.log('You have created a README.md');
-            console.log(response.initialization);
-            return response.initialization  //returns the value of the confirmation: true or false
+            writeContent();  //returns the value of the confirmation: true or false
         } else {
             console.log('You have chosen to create a README.md');
             return Promise.resolve();   // ends the inquiry if user chooses 'no'
         }
     })
 }
+
+function writeContent(){
+    inquirer.prompt(questions).then ( (response) => {
+        fs.writeFileSync("README.md", README(response)) 
+    })
+}
+
+
+function licenseBadge(License) {
+    //takes a license argument, and passes it in the switch parameter. will then return the corresponding badge of the license
+    switch(License) {
+        case "MIT License": return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+        case "Apache License 2.0": return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+        case "BSD 2-Clause License" : return `![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)`;
+        case "BSD 3-Clause License": return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+        case "Mozilla Public License (MPL)": return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
+        case "zlib License": return `![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)`
+        case "Eclipse Public License": return `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)`;
+
+    }
+}
+
 function README({ title, description, ToC, Installation, Usage, License, Contributing, Tests, Questions }) {
-    return `# ${title}
+    var badge = licenseBadge(License);  //calls the licenseBadge function and gets the badge of the license passed in the parameters
+    console.log(badge);
+
+    return `
+${badge}
+
+# ${title}
 
 ## description
 ${description}
