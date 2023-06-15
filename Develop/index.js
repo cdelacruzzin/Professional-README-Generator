f40118f8bff1ed0a587263ea6d59149c0c69fb5d
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-06-15 00:06:31 -0400

    a readme file is created when user answers `yes` to the first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'confirm',
+        message: 'Do you want to create a README file?',
+        name: 'initialization'
+    }
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then( (response) => {
 
+        if(response.initialization) {
+            fs.writeFileSync("main.md", README())
+            console.log(response.initialization)
+        }
+})
+}
+
+function README() {
+    return ` # title`
+
+}
 // Function call to initialize app
 init();
